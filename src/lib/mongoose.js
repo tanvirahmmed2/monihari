18df6f0c420eb56f0eadc1a36614527009f3c796
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-26 20:34:34 +0600

    model

--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const ConnectDB=async () => {
+export const ConnectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI)
         console.log("successfully connected to mongoDB")
---
+++ src/models/user.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    }
+
+})
+
+const User= mongoose.model('users', userSchema)
+
+
+export default User
