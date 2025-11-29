1cf52560e70e21029922981ac1b473d6a4ed9b37
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-29 15:50:18 +0600

    login page

---
+++ src/app/(user)/login/layout.jsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "Login",
+  description: "Log in to your accoun to access more data",
+};
+
+
+const RootLayout = ({children}) => {
+  return (
+    <>
+    {children}
+    </>
+  )
+}
+
+export default RootLayout
--- src/app/(user)/login/page.jsx
+++ src/app/(user)/login/page.jsx
@@ -1,10 +1,34 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      Login
-    </div>
+    <section className='w-full h-screen flex items-center justify-center'>
+
+      <div className='w-auto p-6 flex flex-col md:flex-row items-center justify-center gap-4 shadow-2xl rounded-3xl'>
+        <div className='w-auto flex flex-col justify-center gap-2 p-2'>
+          <p className='text-xl font-semibold'>Welcome Back to</p>
+          <h1 className='text-4xl font-semibold'>Shop</h1>
+          <p>Login to your account</p>
+          <p>enjoy our premium services</p>
+          <Link href='/register' className='mx-auto italic text-green-400'>register</Link>
+        </div>
+
+        <form className='w-auto flex flex-col justify-center gap-4'>
+          <div className='w-full flex flex-col gap-2'>
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' name='email' className='px-2 p-1 border-2 rounded-lg outline-none border-black/15' />
+          </div>
+          <div className='w-full flex flex-col gap-2'>
+            <label htmlFor="password">Password</label>
+            <input type="password" id='password' name='password' className='px-2 p-1 border-2 rounded-lg outline-none border-black/15' />
+          </div>
+          <button type='submit'>Login</button>
+        </form>
+
+      </div>
+
+    </section>
   )
 }
 
