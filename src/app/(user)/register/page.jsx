f3c0fc62bf58fbeddced3feb656494ce6aa0f539
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-29 20:03:34 +0600

    register api setup

--- src/app/(user)/register/page.jsx
+++ src/app/(user)/register/page.jsx
@@ -1,9 +1,34 @@
 'use client'
 
+import axios from 'axios'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+
+  const [data, setData]= useState({
+    name: '',
+    email: '',
+    phone: '',
+    password:''
+
+  })
+
+  const handleChange= (e)=>{
+    const {name, value}= e.target
+    setData((prev)=>({...prev, [name]: value}))
+  }
+
+  const handleRegister=async (e) => {
+    e.preventDefault()
+    try {
+      const response= await axios.post('http://localhost:3000/api/user/register', data, {withCredentials: true})
+      alert(response.data.message)
+    } catch (error) {
+      console.log(error)
+    }
+    
+  }
   return (
      <section className='w-full min-h-screen flex items-center justify-center'>
 
@@ -15,22 +40,22 @@ const Register = () => {
           <Link href='/login' className='mx-auto italic text-green-400 text-xs'>Already registered?</Link>
         </div>
 
-        <form className='w-auto flex flex-col justify-center gap-4'>
+        <form onSubmit={handleRegister} className='w-auto flex flex-col justify-center gap-4'>
           <div className='w-full flex flex-col gap-2'>
             <label htmlFor="name">Name</label>
-            <input type="text" id='name' name='name' className='px-2 p-1 border-2 rounded-lg outline-none border-black/15' />
+            <input type="text" id='name' name='name' required onChange={handleChange} value={data.name} className='px-2 p-1 border-2 rounded-lg outline-none border-black/15' />
           </div>
           <div className='w-full flex flex-col gap-2'>
             <label htmlFor="phone">Phone</label>
-            <input type="text" id='phone' name='phone' className='px-2 p-1 border-2 rounded-lg outline-none border-black/15' />
+            <input type="text" id='phone' name='phone' required onChange={handleChange} value={data.phone} className='px-2 p-1 border-2 rounded-lg outline-none border-black/15' />
           </div>
           <div className='w-full flex flex-col gap-2'>
             <label htmlFor="email">Email</label>
-            <input type="email" id='email' name='email' className='px-2 p-1 border-2 rounded-lg outline-none border-black/15' />
+            <input type="email" id='email' name='email'  required onChange={handleChange} value={data.email} className='px-2 p-1 border-2 rounded-lg outline-none border-black/15' />
           </div>
           <div className='w-full flex flex-col gap-2'>
             <label htmlFor="password">Password</label>
-            <input type="password" id='password' name='password' className='px-2 p-1 border-2 rounded-lg outline-none border-black/15' />
+            <input type="password" id='password' name='password' required onChange={handleChange} value={data.password} className='px-2 p-1 border-2 rounded-lg outline-none border-black/15' />
           </div>
           <button type='submit' className='bg-black text-white p-1 rounded-lg cursor-pointer'>Register</button>
         </form>
