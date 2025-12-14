b61ec1b928bdb0c13bc76623b9547ea8ec8d5a44
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-12-14 14:06:16 +0600

    home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,10 @@ import React from 'react'
 const page = () => {
   return (
     <div className='w-full min-h-screen flex flex-col items-center justify-center gap-2'>
-      <div className='w-full min-h-[700px] flex-col flex items-center justify-center gap-5'>
-        
+      <div className='w-full min-h-[700px] flex-col md:flex-row flex items-center justify-center gap-5'>
+        <div>
+          <h1>Monihari</h1>
+        </div>
         
       </div>
       <Featured/>
